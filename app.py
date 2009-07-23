5f7868c2b19f666e1ee69ec978abfcbda3ae1cff
Yuya Kato <yuyakato@gmail.com>
2009-07-24 01:07:22 +0900

    リファクタリング

--- app.py
+++ app.py
@@ -32,29 +32,7 @@ print ""
 time  = nowcast.get_current_observed_time()
 image = nowcast.get_image(211, time, 0)
 
-
-pngc = png.PngContainer.load(image)
-print pngc
-print pngc.chunks
-
-header_chunk = pngc.first_chunk_by_type("IHDR")
-header = png.Header.load(header_chunk.data)
-print header_chunk
-print header
-
-pal_chunk = pngc.first_chunk_by_type("PLTE")
-pal = png.Palette.load(pal_chunk.data)
-print pal_chunk
-print pal
-
-data_chunk = pngc.joined_chunk_by_type("IDAT")
-bitmap = png.BitmapFor8bitPalette.load(zlib.decompress(data_chunk.data), header.width, header.height)
-print data_chunk
-print len(data_chunk.data)
-print len(zlib.decompress(data_chunk.data))
-
-print "---"
 png = png.Png8bitPalette.load(image)
 print png
-for line in png.bitmap.bitmap:
-  print ",".join(["%02X" % x for x in line])
+#for line in png.bitmap.bitmap:
+#  print ",".join(["%02X" % x for x in line])
