8a6e68e7e6180f2d7322de0772da3588bd4da7cf
Yuya Kato <yuyakato@gmail.com>
2009-07-24 03:17:49 +0900

    グリッドの位置を抽出する処理を追加

--- app.py
+++ app.py
@@ -33,5 +33,29 @@ image = nowcast.get_image(211, time, 0)
 
 png = png.Png8bitPalette.load(image)
 print png
+
+dx = png.bitmap.width
+dy = png.bitmap.height
+bitmap = png.bitmap.bitmap
+print dx
+print dy
+print bitmap
+
+cindex = None
+for i in range(0, len(png.palette.colors)):
+  if png.palette.colors[i] == (102,102,102):
+    cindex = i
+print cindex
+
+xx = []
+for x in range(0, dx):
+  if bitmap[100][x] == cindex:
+    xx.append(x)
+  if bitmap[200][x] == cindex:
+    xx.append(x)
+  if bitmap[300][x] == cindex:
+    xx.append(x)
+print xx
+
 #for line in png.bitmap.bitmap:
 #  print ",".join(["%02X" % x for x in line])
