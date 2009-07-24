4dd064c1b890172a06fb2528d0204e0bfb05499b
Yuya Kato <yuyakato@gmail.com>
2009-07-24 11:38:57 +0900

    座標変換の処理を追加

--- app.py
+++ app.py
@@ -20,47 +20,63 @@ print ""
 #png = png.Png8bitPalette.load(image)
 #print png
 
+import math
+
 class AreaInfo:
-  def __init__(self, code, xy1, xy2, lnglat1, lnglat2):
-    self.code    = code
-    self.xy1     = xy1
-    self.xy2     = xy2
-    self.lnglat1 = lnglat1
-    self.lnglat2 = lnglat2
+  def __init__(self, code, gxy1, gxy2, glnglat1, glnglat2):
+    self.code     = code
+    self.gxy1     = gxy1
+    self.gxy2     = gxy2
+    self.glnglat1 = glnglat1
+    self.glnglat2 = glnglat2
 
-  def x1(self): return self.xy1[0]
-  def y1(self): return self.xy1[1]
-  def x2(self): return self.xy2[0]
-  def y2(self): return self.xy2[1]
-  def dx(self): return self.x2() - self.x1()
-  def dy(self): return self.y2() - self.y1()
+  def gx1(self): return self.gxy1[0]
+  def gy1(self): return self.gxy1[1]
+  def gx2(self): return self.gxy2[0]
+  def gy2(self): return self.gxy2[1]
+  def gdx(self): return self.gx2() - self.gx1()
+  def gdy(self): return self.gy2() - self.gy1()
 
-  def lng1(self): return self.lnglat1[0]
-  def lat1(self): return self.lnglat1[1]
-  def lng2(self): return self.lnglat2[0]
-  def lat2(self): return self.lnglat2[1]
-  def dlng(self): return self.lng2() - self.lng1()
-  def dlat(self): return self.lat1() - self.lat2()
+  def glng1(self): return self.glnglat1[0]
+  def glat1(self): return self.glnglat1[1]
+  def glng2(self): return self.glnglat2[0]
+  def glat2(self): return self.glnglat2[1]
+  def gdlng(self): return self.glng2() - self.glng1()
+  def gdlat(self): return self.glat1() - self.glat2()
+
+  def lng_to_x(self, lng):
+    return int((lng - self.glng1()) / (float(+self.gdlng()) / self.gdx()) + self.gx1())
+
+  def lat_to_y(self, lat):
+    return int((lat - self.glat2()) / (float(-self.gdlat()) / self.gdy()) + self.gy2())
+
+  def lnglat_to_xy(self, lnglat):
+    return (self.lng_to_x(lnglat[0]), self.lat_to_y(lnglat[1]))
 
 kinki = AreaInfo(
-  code    = 211,
-  xy1     = ( 54,  93),
-  xy2     = (473, 396),
-  lnglat1 = (133, 36),
-  lnglat2 = (138, 33))
+  code     = 211,
+  gxy1     = ( 54,  93),
+  gxy2     = (473, 396),
+  glnglat1 = (133, 36),
+  glnglat2 = (138, 33))
 
 print kinki
-#print kinki.x1()
-#print kinki.y1()
-#print kinki.x2()
-#print kinki.y2()
-#print kinki.dx()
-#print kinki.dy()
-print kinki.lnglat1
-print kinki.lng1()
-print kinki.lat1()
-print kinki.lnglat2
-print kinki.lng2()
-print kinki.lat2()
-print kinki.dlng()
-print kinki.dlat()
+print kinki.gxy1
+#print kinki.gx1()
+#print kinki.gy1()
+print kinki.gxy2
+#print kinki.gx2()
+#print kinki.gy2()
+print kinki.gdx()
+print kinki.gdy()
+print kinki.glnglat1
+#print kinki.glng1()
+#print kinki.glat1()
+print kinki.glnglat2
+#print kinki.glng2()
+#print kinki.glat2()
+print kinki.gdlng()
+print kinki.gdlat()
+print kinki.lng_to_x(135.18359)
+print kinki.lat_to_y(34.67902)
+print kinki.lnglat_to_xy((135.18359, 34.67902))
