ce25c6902383129af6af9d28c2aef1a376758950
Yuya Kato <yuyakato@gmail.com>
2009-07-30 17:28:24 +0900

    イメージハンドラのルーティングを変更

--- image_handler.py
+++ image_handler.py
@@ -7,19 +7,19 @@ from google.appengine.ext.webapp import template
 
 
 class WholeImage(webapp.RequestHandler):
-  def get(self):
+  def get(self, area, date, ordinal):
     pass
 
 class PartialImage(webapp.RequestHandler):
-  def get(self):
+  def get(self, area, date, ordinal, x, y):
     pass
 
 
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
     [
-      (r"/image/", WholeImage),
-      (r"/image/", PartialImage),
+      (r"/image/(\d{3})/(\d{8})/(\d{2})\.png",               WholeImage),
+      (r"/image/(\d{3})/(\d{8})/(\d{2})\.(\d+)\.(\d+)\.gif", PartialImage),
     ],
     debug = True)
   run_wsgi_app(application)
