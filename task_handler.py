be38300b38448d2b4d53a2b4d508ee34716a73e1
Yuya Kato <yuyakato@gmail.com>
2009-07-30 17:32:48 +0900

    タスクハンドラのルーティングを変更

--- task_handler.py
+++ task_handler.py
@@ -6,19 +6,21 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
 
+# for cron
 class CacheClearTask(webapp.RequestHandler):
   def get(self):
     pass
 
 class CacheFetchTask(webapp.RequestHandler):
-  def get(self):
+  def get(self, area, date, ordinal):
     pass
 
 
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
     [
-      (r"/task/", CacheClearTask),
+      (r"/task/clear",                         CacheClearTask),
+      (r"/task/fetch/(\d{3})/(\d{8})/(\d{2})", CacheFetchTask),
     ],
     debug = True)
   run_wsgi_app(application)
