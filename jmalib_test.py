6fb158b6ae75b1bc4ec28c79b475dcce4af18cdb
Yuya Kato <yuyakato@gmail.com>
2009-08-06 13:57:27 +0900

    リファクタリング

--- jmalib_test.py
+++ jmalib_test.py
@@ -8,50 +8,50 @@ import jmalib
 
 class TestTimeUtility(unittest.TestCase):
   def setUp(self):
-    pass
+    self.klass = jmalib.TimeUtility
 
   def test_get_per_minute_time(self):
     self.assertEqual(
       datetime.datetime(2000, 1, 1, 0, 0),
-      jmalib.TimeUtility.get_per_minute_time(datetime.datetime(2000, 1, 1, 0, 0, 0)))
+      self.klass.get_per_minute_time(datetime.datetime(2000, 1, 1, 0, 0, 0)))
     self.assertEqual(
       datetime.datetime(1999, 12, 31, 23, 59),
-      jmalib.TimeUtility.get_per_minute_time(datetime.datetime(1999, 12, 31, 23, 59, 59)))
+      self.klass.get_per_minute_time(datetime.datetime(1999, 12, 31, 23, 59, 59)))
 
 
 class TestRadarNowCast(unittest.TestCase):
   def setUp(self):
-    pass
+    self.klass = jmalib.RadarNowCast
 
   def test_create_radar_image_url(self):
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/radar/000/200001010000-00.png",
-      jmalib.RadarNowCast.create_radar_image_url(0, datetime.datetime(2000, 1, 1, 0, 0)))
+      self.klass.create_radar_image_url(0, datetime.datetime(2000, 1, 1, 0, 0)))
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/radar/999/199912312359-00.png",
-      jmalib.RadarNowCast.create_radar_image_url(999, datetime.datetime(1999, 12, 31, 23, 59)))
+      self.klass.create_radar_image_url(999, datetime.datetime(1999, 12, 31, 23, 59)))
 
   def test_create_nowcast_image_url(self):
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/nowcast/000/200001010000-00.png",
-      jmalib.RadarNowCast.create_nowcast_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 0))
+      self.klass.create_nowcast_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 0))
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/nowcast/999/199912312359-99.png",
-      jmalib.RadarNowCast.create_nowcast_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99))
+      self.klass.create_nowcast_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99))
 
   def test_create_image_url(self):
     self.assertEqual(
-      jmalib.RadarNowCast.create_radar_image_url(0, datetime.datetime(2000, 1, 1, 0, 0)),
-      jmalib.RadarNowCast.create_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 0))
+      self.klass.create_radar_image_url(0, datetime.datetime(2000, 1, 1, 0, 0)),
+      self.klass.create_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 0))
     self.assertEqual(
-      jmalib.RadarNowCast.create_radar_image_url(999, datetime.datetime(1999, 12, 31, 23, 59)),
-      jmalib.RadarNowCast.create_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 0))
+      self.klass.create_radar_image_url(999, datetime.datetime(1999, 12, 31, 23, 59)),
+      self.klass.create_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 0))
     self.assertEqual(
-      jmalib.RadarNowCast.create_nowcast_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 1),
-      jmalib.RadarNowCast.create_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 1))
+      self.klass.create_nowcast_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 1),
+      self.klass.create_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 1))
     self.assertEqual(
-      jmalib.RadarNowCast.create_nowcast_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99),
-      jmalib.RadarNowCast.create_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99))
+      self.klass.create_nowcast_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99),
+      self.klass.create_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99))
 
 if __name__ == "__main__":
   unittest.main()
