446c574716a9ab06f5dfda928f7fd4edefb3ccf4
Yuya Kato <yuyakato@gmail.com>
2009-08-06 15:14:37 +0900

    リファクタリング

--- nowcast.py
+++ nowcast.py
@@ -16,6 +16,15 @@ def get_jst_now():
 def get_per_minute_time(time):
   return datetime.datetime(time.year, time.month, time.day, time.hour, time.minute)
 
+def fetcher(url):
+  logging.info("fetch " + url)
+  result = urlfetch.fetch(url)
+  if result.status_code == 200:
+    return result.content
+  else:
+    # TODO: raise exception
+    return None
+
 def get_current_observed_time():
   current_time = get_per_minute_time(get_jst_now())
 
@@ -25,15 +34,6 @@ def get_current_observed_time():
     return caches[0].observed_time
 
   else:
-    def fetcher(url):
-      logging.info("fetch " + url)
-      result = urlfetch.fetch(url)
-      if result.status_code == 200:
-        return result.content
-      else:
-        # TODO: raise exception
-        return None
-
     observed_time = jmalib.RadarNowCast.get_current_radar_time(fetcher)
 
     cache = model.ObservedTimeCache(
@@ -53,15 +53,6 @@ def get_current_predictive_time():
     return caches[0].predictive_time
 
   else:
-    def fetcher(url):
-      logging.info("fetch " + url)
-      result = urlfetch.fetch(url)
-      if result.status_code == 200:
-        return result.content
-      else:
-        # TODO: raise exception
-        return None
-
     predictive_time = jmalib.RadarNowCast.get_current_nowcast_time(fetcher)
 
     cache = model.PredictiveTimeCache(
@@ -72,26 +63,20 @@ def get_current_predictive_time():
 
     return predictive_time
 
-def create_image_url(area, time, no):
-  return jmalib.RadarNowCast.create_image_url(area, time, no)
-
 def get_image(area, time, ordinal):
   caches = db.GqlQuery("SELECT * FROM ImageCache WHERE area = :1 AND time = :2 AND ordinal = :3", area, time, ordinal)
 
   if caches.count() > 0:
     cache = caches[0]
   else:
-    url = create_image_url(area, time, ordinal)
-    logging.info("fetch " + url)
-    result = urlfetch.fetch(url)
-    if result.status_code == 200:
-      cache = model.ImageCache(
-        area        = area,
-        time        = time,
-        ordinal     = ordinal,
-        image       = db.Blob(result.content),
-        expire_time = get_jst_now() + datetime.timedelta(minutes = 20))
-      cache.put()
-    #else:
+    image = jmalib.RadarNowCast.get_image(area, time, ordinal, fetcher)
+
+    cache = model.ImageCache(
+      area        = area,
+      time        = time,
+      ordinal     = ordinal,
+      image       = db.Blob(image),
+      expire_time = get_jst_now() + datetime.timedelta(minutes = 20))
+    cache.put()
 
   return cache.image
