6446178dd0c4a768f55a0982bf4ede74ca355bc9
Yuya Kato <yuyakato@gmail.com>
2009-08-06 11:45:18 +0900

    各種クラスの雛形と、低レベルヘッダクラスの初期化処理を追加

--- giflib.py
+++ giflib.py
@@ -1,1 +1,26 @@
 # -*- coding: utf-8 -*-
+
+# 高レベル イメージクラス
+class Image:
+  def __init__(self):
+    pass
+
+# 高レベル パレットクラス
+class Palette:
+  def __init__(self):
+    pass
+
+# 高レベル ビットマップクラス
+class Bitmap:
+  def __init__(self):
+    pass
+
+# 低レベル GIFヘッダ
+class RawHeader:
+  def __init__(self):
+    self.signature                  = "GIF87a"
+    self.width                      = None
+    self.height                     = None
+    self.color_resolution           = 8
+    self.is_sorted_color_table      = False
+    self.size_of_global_color_table = 0
--- giflib_test.py
+++ giflib_test.py
@@ -4,9 +4,18 @@ import unittest
 
 import giflib
 
-class TestXXX(unittest.TestCase):
-  def test_hoge(self):
-    self.assertEqual(1, 1)
+class TestRawHeader(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def test_init(self):
+    obj = giflib.RawHeader()
+    self.assertEqual("GIF87a", obj.signature)
+    self.assertEqual(None,     obj.width)
+    self.assertEqual(None,     obj.height)
+    self.assertEqual(8,        obj.color_resolution)
+    self.assertEqual(False,    obj.is_sorted_color_table)
+    self.assertEqual(0,        obj.size_of_global_color_table)
 
 if __name__ == "__main__":
   unittest.main()
