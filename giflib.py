07d876f5ef79d7743a71d22f5777b377b6a95905
Yuya Kato <yuyakato@gmail.com>
2009-08-07 02:20:12 +0900

    高レベルGIFライブラリの雛形を追加

---
+++ giflib.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# 高レベル ビットマップクラス
+class Bitmap:
+  def __init__(self, width, height):
+    self.width  = width
+    self.height = height
---
+++ giflib_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import giflib
+
+
+class TestBitmap(unittest.TestCase):
+  def setUp(self):
+    pass
+
+
+if __name__ == "__main__":
+  unittest.main()
