240355b0a8ea7ace3c47abb140a9266c38108b4c
Yuya Kato <yuyakato@gmail.com>
2009-08-06 12:09:16 +0900

    GIFヘッダのフラグの処理を追加

--- giflib.py
+++ giflib.py
@@ -21,6 +21,33 @@ class RawHeader:
     self.signature                  = "GIF87a"
     self.width                      = None
     self.height                     = None
-    self.color_resolution           = 8
+    self.color_resolution           = 8 # bits
     self.is_sorted_color_table      = False
-    self.size_of_global_color_table = 0
+    self.size_of_global_color_table = 0 # bits
+
+  def flag(self):
+    bits = ""
+    bits += "0"
+
+    bits += "0"
+    bits += "000"
+
+    return int(bits, 2)
+
+  def has_global_color_table_flag(self):
+    if self.size_of_global_color_table == 0:
+      return 0
+    else:
+      return 1
+
+  def color_resolution_flag(self):
+    return self.color_resolution - 1
+
+  def is_sorted_color_table_flag(self):
+    return 1 if self.is_sorted_color_table else 0
+
+  def size_of_global_color_table_flag(self):
+    if self.size_of_global_color_table == 0:
+      return 0
+    else:
+      return self.size_of_global_color_table - 1
--- giflib_test.py
+++ giflib_test.py
@@ -10,12 +10,58 @@ class TestRawHeader(unittest.TestCase):
 
   def test_init(self):
     obj = giflib.RawHeader()
+
     self.assertEqual("GIF87a", obj.signature)
     self.assertEqual(None,     obj.width)
     self.assertEqual(None,     obj.height)
     self.assertEqual(8,        obj.color_resolution)
     self.assertEqual(False,    obj.is_sorted_color_table)
     self.assertEqual(0,        obj.size_of_global_color_table)
+
+  def test_flag(self):
+    obj = giflib.RawHeader()
+
+    obj.color_resolution           = 1
+    obj.is_sorted_color_table      = False
+    obj.size_of_global_color_table = 0
+    self.assertEqual(int("00000000", 2), obj.flag())
+
+    #obj.color_resolution           = 2
+    #obj.is_sorted_color_table      = False
+    #obj.size_of_global_color_table = 0
+    #self.assertEqual(int("00010000", 2), obj.flag())
+
+  def test_has_global_color_table_flag(self):
+    obj = giflib.RawHeader()
+    obj.size_of_global_color_table = 0
+    self.assertEqual(0, obj.has_global_color_table_flag())
+    obj.size_of_global_color_table = 1
+    self.assertEqual(1, obj.has_global_color_table_flag())
+    obj.size_of_global_color_table = 8
+    self.assertEqual(1, obj.has_global_color_table_flag())
+
+  def test_color_resolution_flag(self):
+    obj = giflib.RawHeader()
+    obj.color_resolution = 1
+    self.assertEqual(0, obj.color_resolution_flag())
+    obj.color_resolution = 8
+    self.assertEqual(7, obj.color_resolution_flag())
+
+  def test_is_sorted_color_table_flag(self):
+    obj = giflib.RawHeader()
+    obj.is_sorted_color_table = False
+    self.assertEqual(0, obj.is_sorted_color_table_flag())
+    obj.is_sorted_color_table = True
+    self.assertEqual(1, obj.is_sorted_color_table_flag())
+
+  def test_size_of_global_color_table_flag(self):
+    obj = giflib.RawHeader()
+    obj.size_of_global_color_table = 0
+    self.assertEqual(0, obj.size_of_global_color_table_flag())
+    obj.size_of_global_color_table = 1
+    self.assertEqual(0, obj.size_of_global_color_table_flag())
+    obj.size_of_global_color_table = 8
+    self.assertEqual(7, obj.size_of_global_color_table_flag())
 
 if __name__ == "__main__":
   unittest.main()
