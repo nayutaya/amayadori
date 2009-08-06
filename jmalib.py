17afd380c10f712a0a7b94b40210214e66343295
Yuya Kato <yuyakato@gmail.com>
2009-08-06 13:39:07 +0900

    リファクタリング

--- jmalib.py
+++ jmalib.py
@@ -4,21 +4,18 @@ import datetime
 
 class RadarNowCast:
   @classmethod
-  def create_observed_image_url(cls, area, time):
-    url  = "http://www.jma.go.jp/jp/radnowc/imgs/radar/"
-    url += ("%03i" % area)
-    url += "/"
-    url += time.strftime("%Y%m%d%H%M")
+  def create_radar_image_url(cls, area, time):
+    url  = "http://www.jma.go.jp/jp/radnowc/imgs/radar"
+    url += "/" + ("%03i" % area)
+    url += "/" + time.strftime("%Y%m%d%H%M")
     url += "-00.png"
     return url
 
   @classmethod
-  def create_predictive_image_url(cls, area, time, no):
-    url = "http://www.jma.go.jp/jp/radnowc/imgs/nowcast/"
-    url += ("%03i" % area)
-    url += "/"
-    url += time.strftime("%Y%m%d%H%M")
-    url += "-"
-    url += ("%02i" % no)
+  def create_nowcast_image_url(cls, area, time, no):
+    url = "http://www.jma.go.jp/jp/radnowc/imgs/nowcast"
+    url += "/" + ("%03i" % area)
+    url += "/" + time.strftime("%Y%m%d%H%M")
+    url += "-" + ("%02i" % no)
     url += ".png"
     return url
--- jmalib_test.py
+++ jmalib_test.py
@@ -10,21 +10,22 @@ class TestRadarNowCast(unittest.TestCase):
   def setUp(self):
     pass
 
-  def test_create_observed_image_url(self):
+  def test_create_radar_image_url(self):
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/radar/000/200001010000-00.png",
-      jmalib.RadarNowCast.create_observed_image_url(0, datetime.datetime(2000, 1, 1, 0, 0)))
+      jmalib.RadarNowCast.create_radar_image_url(0, datetime.datetime(2000, 1, 1, 0, 0)))
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/radar/999/199912312359-00.png",
-      jmalib.RadarNowCast.create_observed_image_url(999, datetime.datetime(1999, 12, 31, 23, 59)))
+      jmalib.RadarNowCast.create_radar_image_url(999, datetime.datetime(1999, 12, 31, 23, 59)))
 
-  def test_create_predictive_image_url(self):
+  def test_create_nowcast_image_url(self):
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/nowcast/000/200001010000-00.png",
-      jmalib.RadarNowCast.create_predictive_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 0))
+      jmalib.RadarNowCast.create_nowcast_image_url(0, datetime.datetime(2000, 1, 1, 0, 0), 0))
     self.assertEqual(
       "http://www.jma.go.jp/jp/radnowc/imgs/nowcast/999/199912312359-99.png",
-      jmalib.RadarNowCast.create_predictive_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99))
+      jmalib.RadarNowCast.create_nowcast_image_url(999, datetime.datetime(1999, 12, 31, 23, 59), 99))
+
 
 if __name__ == "__main__":
   unittest.main()
