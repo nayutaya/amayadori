ce877ccd6e14db51a75605c613f9b61cb096092b
Yuya Kato <yuyakato@gmail.com>
2009-08-06 11:32:07 +0900

    GIFライブラリの雛形を追加

---
+++ giflib_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import giflib
+
+class TestXXX(unittest.TestCase):
+  def test_hoge(self):
+    self.assertEqual(1, 1)
+
+if __name__ == "__main__":
+  unittest.main()
