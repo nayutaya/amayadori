1349c2d8e9b0d10874f766a72ff86c3063fd2d03
Yuya Kato <yuyakato@gmail.com>
2009-08-06 13:27:31 +0900

    気象庁関連ライブラリの雛形を追加

---
+++ jmalib_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import jmalib
+
+
+class TestRadarNowCast(unittest.TestCase):
+  def setUp(self):
+    pass
+
+
+if __name__ == "__main__":
+  unittest.main()
