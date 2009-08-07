906766709f288373cd647a54ce8e0fe23a0cb756
Yuya Kato <yuyakato@gmail.com>
2009-08-07 11:48:12 +0900

    リファクタリング

--- imglib.py
+++ imglib.py
@@ -9,11 +9,6 @@ class BitmapBase:
     self.height = height
     self.pixels = [init_pixel for i in xrange(self.width * self.height)]
 
-# インデックスカラービットマップクラス
-class IndexBitmap(BitmapBase):
-  def __init__(self, width, height):
-    BitmapBase.__init__(self, width, height, 0)
-
   def get_pixels(self):
     return self.pixels[:]
 
@@ -29,25 +24,15 @@ class IndexBitmap(BitmapBase):
   def set_pixel(self, x, y, pixel):
     self.pixels[y * self.width + x] = pixel
     return self
+
+
+# インデックスカラービットマップクラス
+class IndexBitmap(BitmapBase):
+  def __init__(self, width, height):
+    BitmapBase.__init__(self, width, height, 0)
 
 
 # RGBカラービットマップクラス
 class RgbBitmap(BitmapBase):
   def __init__(self, width, height):
     BitmapBase.__init__(self, width, height, (0, 0, 0))
-
-  def get_pixels(self):
-    return self.pixels[:]
-
-  def set_pixels(self, pixels):
-    if len(pixels) != self.width * self.height:
-      raise ValueError, "invalid size"
-    self.pixels = pixels[:]
-    return self
-
-  def get_pixel(self, x, y):
-    return self.pixels[y * self.width + x]
-
-  def set_pixel(self, x, y, pixel):
-    self.pixels[y * self.width + x] = pixel
-    return self
