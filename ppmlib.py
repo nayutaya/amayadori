6a26e766f58e18a03b01360f774b7d3441343b32
Yuya Kato <yuyakato@gmail.com>
2009-08-07 12:05:13 +0900

    最小限のPPM書き込み処理を実装

--- ppmlib.py
+++ ppmlib.py
@@ -2,6 +2,21 @@
 
 import imglib
 
+# ASCII形式PPMファイル書き込みクラス
 class PpmWriter:
   def __init__(self, bitmap):
     self.bitmap = bitmap
+
+  def write(self, io):
+    io.write("P3\n")
+    io.write(str(self.bitmap.width))
+    io.write(" ")
+    io.write(str(self.bitmap.height))
+    io.write("\n")
+    io.write("255\n")
+
+    r, g, b = self.bitmap.get_pixel(0, 0)
+    io.write(str(r) + " " + str(g) + " " + str(b))
+    io.write("\n")
+
+    return self
--- ppmlib_test.py
+++ ppmlib_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import StringIO
 
 import ppmlib
 import imglib
@@ -14,6 +15,19 @@ class TestPpmWriter(unittest.TestCase):
     bitmap = imglib.RgbBitmap(10, 5)
     obj = self.klass(bitmap)
     self.assertEqual(bitmap, obj.bitmap)
+
+  def test_write__minimum(self):
+    sio = StringIO.StringIO()
+    bitmap = imglib.RgbBitmap(1, 1)
+    bitmap.set_pixel(0, 0, (1, 2, 3))
+    obj = self.klass(bitmap)
+    obj.write(sio)
+
+    expected  = "P3\n"
+    expected += "1 1\n"
+    expected += "255\n"
+    expected += "1 2 3\n"
+    self.assertEqual(expected, sio.getvalue())
 
 if __name__ == "__main__":
   unittest.main()
