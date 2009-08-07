a352c7f03a6bffe47740b052ddd4bac1888d7331
Yuya Kato <yuyakato@gmail.com>
2009-08-07 11:21:52 +0900

    インデックスビットマップの初期化処理、ピクセル列の設定/取得処理を追加

--- imglib.py
+++ imglib.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+# 汎用イメージライブラリ
+
+class IndexBitmap:
+  def __init__(self, width, height):
+    self.width  = width
+    self.height = height
+    self.pixels = [0 for i in xrange(self.width * self.height)]
+
+  def get_pixels(self):
+    return self.pixels[:]
+
+  def set_pixels(self, pixels):
+    self.pixels = pixels[:]
+    return self
--- imglib_test.py
+++ imglib_test.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+
+import unittest
+
+import imglib
+
+
+class TestIndexBitmap(unittest.TestCase):
+  def setUp(self):
+    self.klass = imglib.IndexBitmap
+
+  def test_init(self):
+    obj = self.klass(10, 20)
+    self.assertEqual(10, obj.width)
+    self.assertEqual(20, obj.height)
+    self.assertEqual(
+      [0 for i in xrange(10 * 20)],
+      obj.pixels)
+
+  def test_get_pixels(self):
+    obj = self.klass(10, 5)
+    self.assertEqual(
+      [0 for i in range(10 * 5)],
+      obj.get_pixels())
+
+  def test_set_pixels(self):
+    pixels = [i for i in range(10 * 5)]
+    obj = self.klass(10, 5)
+    obj.set_pixels(pixels)
+    self.assertEqual(pixels, obj.get_pixels())
+
+
+if __name__ == "__main__":
+  unittest.main()
