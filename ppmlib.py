1c1613f68fb2236342c098a4839994f27537a748
Yuya Kato <yuyakato@gmail.com>
2009-08-07 11:57:49 +0900

    PPM書き込みクラスの初期化処理を追加

--- ppmlib.py
+++ ppmlib.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+import imglib
+
+class PpmWriter:
+  def __init__(self, bitmap):
+    self.bitmap = bitmap
--- ppmlib_test.py
+++ ppmlib_test.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+
+import unittest
+
+import ppmlib
+import imglib
+
+
+class TestPpmWriter(unittest.TestCase):
+  def setUp(self):
+    self.klass = ppmlib.PpmWriter
+
+  def test_init(self):
+    bitmap = imglib.RgbBitmap(10, 5)
+    obj = self.klass(bitmap)
+    self.assertEqual(bitmap, obj.bitmap)
+
+if __name__ == "__main__":
+  unittest.main()
