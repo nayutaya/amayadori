2ffc8090dd130385f1cf1501379d7026d37bb58b
Yuya Kato <yuyakato@gmail.com>
2009-08-07 11:41:17 +0900

    RGBカラービットマップクラスを追加

--- imglib.py
+++ imglib.py
@@ -2,11 +2,38 @@
 
 # 汎用イメージライブラリ
 
+# インデックスカラービットマップ
 class IndexBitmap:
   def __init__(self, width, height):
     self.width  = width
     self.height = height
     self.pixels = [0 for i in xrange(self.width * self.height)]
+
+  def get_pixels(self):
+    return self.pixels[:]
+
+  def set_pixels(self, pixels):
+    if len(pixels) != self.width * self.height:
+      raise ValueError, "invalid size"
+    self.pixels = pixels[:]
+    return self
+
+  def get_pixel(self, x, y):
+    return self.pixels[y * self.width + x]
+
+  def set_pixel(self, x, y, pixel):
+    self.pixels[y * self.width + x] = pixel
+    return self
+
+
+# RGBカラービットマップ
+class RgbBitmap:
+  def __init__(self, width, height):
+    init_pixel = (0, 0, 0)
+
+    self.width  = width
+    self.height = height
+    self.pixels = [init_pixel for i in xrange(self.width * self.height)]
 
   def get_pixels(self):
     return self.pixels[:]
--- imglib_test.py
+++ imglib_test.py
@@ -63,5 +63,61 @@ class TestIndexBitmap(unittest.TestCase):
     self.assertEqual(expected, obj.get_pixels())
 
 
+class TestRgbBitmap(unittest.TestCase):
+  def setUp(self):
+    self.klass = imglib.RgbBitmap
+
+  def test_init(self):
+    obj = self.klass(10, 20)
+    self.assertEqual(10, obj.width)
+    self.assertEqual(20, obj.height)
+    self.assertEqual(
+      [(0, 0, 0) for i in xrange(10 * 20)],
+      obj.pixels)
+
+  def test_get_pixels(self):
+    obj = self.klass(10, 5)
+    self.assertEqual(
+      [(0, 0, 0) for i in range(10 * 5)],
+      obj.get_pixels())
+
+  def test_set_pixels(self):
+    pixels = [(i, 0, 0) for i in range(10 * 5)]
+    obj = self.klass(10, 5)
+    obj.set_pixels(pixels)
+    self.assertEqual(pixels, obj.get_pixels())
+
+  def test_set_pixels__invalid_size(self):
+    obj = self.klass(10, 5)
+    self.assertRaises(ValueError, obj.set_pixels, [(0, 0, 0)])
+
+  def test_get_pixel(self):
+    obj = self.klass(4, 3)
+    obj.set_pixels([
+      (1, 0, 0), (0, 0, 0), (0, 0, 0), (3, 0, 0),
+      (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0),
+      (4, 0, 0), (0, 0, 0), (0, 0, 0), (2, 0, 0),
+    ])
+
+    self.assertEqual((1, 0, 0), obj.get_pixel(0, 0))
+    self.assertEqual((2, 0, 0), obj.get_pixel(3, 2))
+    self.assertEqual((3, 0, 0), obj.get_pixel(3, 0))
+    self.assertEqual((4, 0, 0), obj.get_pixel(0, 2))
+
+  def test_set_pixel(self):
+    obj = self.klass(4, 3)
+    obj.set_pixel(0, 0, (1, 0, 0))
+    obj.set_pixel(3, 2, (2, 0, 0))
+    obj.set_pixel(3, 0, (3, 0, 0))
+    obj.set_pixel(0, 2, (4, 0, 0))
+
+    expected = [
+      (1, 0, 0), (0, 0, 0), (0, 0, 0), (3, 0, 0),
+      (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0),
+      (4, 0, 0), (0, 0, 0), (0, 0, 0), (2, 0, 0),
+    ]
+    self.assertEqual(expected, obj.get_pixels())
+
+
 if __name__ == "__main__":
   unittest.main()
