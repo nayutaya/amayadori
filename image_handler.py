30f649714f3bdb4c0da5358a198fb47f891cc738
Yuya Kato <yuyakato@gmail.com>
2009-08-11 01:11:01 +0900

    観測画像、予想画像の出力処理をイメージハンドラに移動

--- image_handler.py
+++ image_handler.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import os
+import datetime
+import re
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
+import nowcast
+
+class TimeUtility:
+  @classmethod
+  def yyyymmddhhnn_to_datetime(cls, str):
+    regexp = re.compile(r"^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})$")
+    match  = regexp.match(str)
+    return datetime.datetime(
+        int(match.group(1)), int(match.group(2)), int(match.group(3)),
+        int(match.group(4)), int(match.group(5)))
 
 class IndexPage(webapp.RequestHandler):
   def get(self, area):
@@ -14,21 +27,28 @@ class IndexPage(webapp.RequestHandler):
 
 
 class WholeImage(webapp.RequestHandler):
-  def get(self, area, date, ordinal):
-    pass
+  def get(self, area, time, ordinal):
+    area    = int(area)
+    time    = TimeUtility.yyyymmddhhnn_to_datetime(time)
+    ordinal = int(ordinal)
+
+    image = nowcast.get_image(area, time, ordinal)
+
+    self.response.headers["Content-Type"] = "image/png"
+    self.response.out.write(image)
 
 
 class PartialImage(webapp.RequestHandler):
-  def get(self, area, date, ordinal, x, y):
+  def get(self, area, time, ordinal, x, y):
     pass
 
 
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
     [
-      (r"/image/(\d{3})\.html",                              IndexPage),
-      (r"/image/(\d{3})/(\d{8})/(\d{2})\.png",               WholeImage),
-      (r"/image/(\d{3})/(\d{8})/(\d{2})\.(\d+)\.(\d+)\.gif", PartialImage),
+      (r"/image/(\d{3})\.html",                               IndexPage),
+      (r"/image/(\d{3})/(\d{12})/(\d{2})\.png",               WholeImage),
+      (r"/image/(\d{3})/(\d{12})/(\d{2})\.(\d+)\.(\d+)\.gif", PartialImage),
     ],
     debug = True)
   run_wsgi_app(application)
