13fbb73b771d73d864288c76abebf24a064e0843
Yuya Kato <yuyakato@gmail.com>
2009-08-11 00:33:08 +0900

    画像一覧ページの雛形を追加

--- image_handler.py
+++ image_handler.py
@@ -6,9 +6,17 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
 
+class IndexPage(webapp.RequestHandler):
+  def get(self, area):
+    path = os.path.join(os.path.dirname(__file__), "image.html")
+    html = template.render(path, {})
+    self.response.out.write(html)
+
+
 class WholeImage(webapp.RequestHandler):
   def get(self, area, date, ordinal):
     pass
+
 
 class PartialImage(webapp.RequestHandler):
   def get(self, area, date, ordinal, x, y):
@@ -18,6 +26,7 @@ class PartialImage(webapp.RequestHandler):
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
     [
+      (r"/image/(\d{3})\.html",                              IndexPage),
       (r"/image/(\d{3})/(\d{8})/(\d{2})\.png",               WholeImage),
       (r"/image/(\d{3})/(\d{8})/(\d{2})\.(\d+)\.(\d+)\.gif", PartialImage),
     ],
