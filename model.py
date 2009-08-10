6c04622cb65157534b82219f837911f013799301
Yuya Kato <yuyakato@gmail.com>
2009-08-11 02:40:40 +0900

    リファクタリング

--- model.py
+++ model.py
@@ -2,14 +2,6 @@
 
 from google.appengine.ext import db
 
-class ObservedTimeCache(db.Model):
-  current_time  = db.DateTimeProperty(required=True)
-  observed_time = db.DateTimeProperty(required=True)
-
-class PredictiveTimeCache(db.Model):
-  current_time    = db.DateTimeProperty(required=True)
-  predictive_time = db.DateTimeProperty(required=True)
-
 class ImageCache(db.Model):
   area        = db.IntegerProperty(required=True)
   time        = db.DateTimeProperty(required=True)
--- nowcast.py
+++ nowcast.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import datetime
-import re
 import logging
 from google.appengine.ext import db
 from google.appengine.api import urlfetch
 
 import model
 import jmalib
-import timeutil
-
+import amayadori
 
 def fetcher(url):
   logging.info("fetch " + url)
@@ -21,40 +18,10 @@ def fetcher(url):
     return None
 
 def get_current_observed_time():
-  current_time = timeutil.get_per_minute_time(timeutil.get_jst_now())
-
-  caches = db.GqlQuery("SELECT * FROM ObservedTimeCache WHERE current_time = :1", current_time)
-
-  if caches.count() > 0:
-    return caches[0].observed_time
-
-  else:
-    observed_time = jmalib.RadarNowCast.get_current_radar_time(fetcher)
-
-    cache = model.ObservedTimeCache(
-      current_time  = current_time,
-      observed_time = observed_time)
-    cache.put()
-
-    return observed_time
+  return amayadori.get_radar_time()
 
 def get_current_predictive_time():
-  current_time = timeutil.get_per_minute_time(timeutil.get_jst_now())
-
-  caches = db.GqlQuery("SELECT * FROM PredictiveTimeCache WHERE current_time = :1", current_time)
-
-  if caches.count() > 0:
-    return caches[0].predictive_time
-
-  else:
-    predictive_time = jmalib.RadarNowCast.get_current_nowcast_time(fetcher)
-
-    cache = model.PredictiveTimeCache(
-      current_time    = current_time,
-      predictive_time = predictive_time)
-    cache.put()
-
-    return predictive_time
+  return amayadori.get_nowcast_time()
 
 def get_image(area, time, ordinal):
   caches = db.GqlQuery("SELECT * FROM ImageCache WHERE area = :1 AND time = :2 AND ordinal = :3", area, time, ordinal)
