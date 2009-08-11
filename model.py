8948c6171ec7fa3e616fc6917da7a24b374b4dec
Yuya Kato <yuyakato@gmail.com>
2009-08-11 12:13:24 +0900

    不要なモジュール、クラスを削除

--- model.py
+++ model.py
@@ -2,12 +2,6 @@
 
 from google.appengine.ext import db
 
-class ImageCache(db.Model):
-  area        = db.IntegerProperty(required=True)
-  time        = db.DateTimeProperty(required=True)
-  ordinal     = db.IntegerProperty(required=True)
-  image       = db.BlobProperty(required=True)
-
 class Task(db.Model):
   path = db.StringProperty(required=True)
   time = db.DateTimeProperty(required=True)
