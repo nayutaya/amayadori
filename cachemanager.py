49f665636ed58324e7cae14b5439c08362669c50
Yuya Kato <yuyakato@gmail.com>
2009-08-11 11:57:25 +0900

    イメージキャッシュの処理を追加

--- cachemanager.py
+++ cachemanager.py
@@ -13,6 +13,13 @@ class NowcastTime(db.Model):
   nowcast_time = db.DateTimeProperty(required=True)
 
 
+class Image(db.Model):
+  area    = db.IntegerProperty(required=True)
+  time    = db.DateTimeProperty(required=True)
+  ordinal = db.IntegerProperty(required=True)
+  image   = db.BlobProperty(required=True)
+
+
 class CacheManager:
   def __init__(self):
     pass
@@ -34,6 +41,16 @@ class CacheManager:
     return record
 
   @classmethod
+  def create_image(cls, area, time, ordinal, image):
+    record = Image(
+      area    = area,
+      time    = time,
+      ordinal = ordinal,
+      image   = db.Blob(image))
+    record.put()
+    return record
+
+  @classmethod
   def get_radar_time(cls, current_time):
     records = db.GqlQuery("SELECT * FROM RadarTime WHERE current_time = :1", current_time)
     if records.count() > 0:
@@ -50,6 +67,14 @@ class CacheManager:
       return None
 
   @classmethod
+  def get_image(cls, area, time, ordinal):
+    records = db.GqlQuery("SELECT * FROM Image WHERE area = :1 AND time = :2 AND ordinal = :3", area, time, ordinal)
+    if records.count() > 0:
+      return records[0]
+    else:
+      return None
+
+  @classmethod
   def clear_radar_time(cls, current_time):
     records = db.GqlQuery("SELECT * FROM RadarTime WHERE current_time <= :1", current_time)
     db.delete(records)
@@ -58,5 +83,11 @@ class CacheManager:
   @classmethod
   def clear_nowcast_time(cls, current_time):
     records = db.GqlQuery("SELECT * FROM NowcastTime WHERE current_time <= :1", current_time)
+    db.delete(records)
+    return None
+
+  @classmethod
+  def clear_image(cls, time):
+    records = db.GqlQuery("SELECT * FROM Image WHERE time <= :1", time)
     db.delete(records)
     return None
