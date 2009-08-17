030bd21f9079d1f20f065466beaa5f96b3c01b39
Yuya Kato <yuyakato@gmail.com>
2009-08-17 23:08:44 +0900

    imglib.IndexBitmapクラスを使うように変更

--- giflib.py
+++ giflib.py
@@ -1,26 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import gifrawlib
+import imglib
 
 
 # 高レベル ビットマップクラス
 class Bitmap:
   def __init__(self, width, height, depth = 8):
-    self.width  = width
-    self.height = height
+    self.bitmap = imglib.IndexBitmap(width, height)
     self.depth  = depth
-    self.pixels = [0 for i in xrange(self.width * self.height)]
+
+  def width(self):
+    return self.bitmap.width
+
+  def height(self):
+    return self.bitmap.height
+
+  def get_pixels(self):
+    return self.bitmap.get_pixels()
+
+  def set_pixels(self, pixels):
+    self.bitmap.set_pixels(pixels)
+    return self
 
   def get_pixel(self, x, y):
-    return self.pixels[y * self.width + x]
+    return self.bitmap.get_pixel(x, y)
 
   def set_pixel(self, x, y, pixel):
-    self.pixels[y * self.width + x] = pixel
+    self.bitmap.set_pixel(x, y, pixel)
     return self
 
   def create_image_block_data(self):
     data = gifrawlib.UncompressedImageBlockData()
-    for pixel in self.pixels:
+    for pixel in self.get_pixels():
       data.append(pixel)
     return data.bytes()
 
--- giflib_test.py
+++ giflib_test.py
@@ -12,21 +12,18 @@ class TestBitmap(unittest.TestCase):
 
   def test_init(self):
     obj = giflib.Bitmap(10, 20)
-    self.assertEqual(10, obj.width)
-    self.assertEqual(20, obj.height)
+    self.assertEqual(10, obj.width())
+    self.assertEqual(20, obj.height())
     self.assertEqual(8,  obj.depth)
-    self.assertEqual(
-      [0 for i in xrange(10 * 20)],
-      obj.pixels)
 
   def test_get_pixel(self):
     obj = giflib.Bitmap(5, 4)
-    obj.pixels = [
+    obj.set_pixels([
       1, 0, 0, 0, 3,
       0, 0, 0, 0, 0,
       0, 0, 0, 0, 0,
       4, 0, 0, 0, 2,
-    ]
+    ])
 
     self.assertEqual(1, obj.get_pixel(0, 0))
     self.assertEqual(2, obj.get_pixel(4, 3))
@@ -46,14 +43,14 @@ class TestBitmap(unittest.TestCase):
       0, 0, 0, 0, 0,
       4, 0, 0, 0, 2,
     ]
-    self.assertEqual(expected, obj.pixels)
+    self.assertEqual(expected, obj.get_pixels())
 
   def test_create_image_block_data(self):
     obj = giflib.Bitmap(3, 2)
-    obj.pixels = [0, 1, 2, 3, 4, 5]
+    obj.set_pixels([0, 1, 2, 3, 4, 5])
 
     data = gifrawlib.UncompressedImageBlockData()
-    for byte in obj.pixels:
+    for byte in obj.get_pixels():
       data.append(byte)
     expected = data.bytes()
     self.assertEqual(expected, obj.create_image_block_data())
