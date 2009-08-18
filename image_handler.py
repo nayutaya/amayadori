7cbc8b25d1f8f0b0963c3c147f3b586246360e8d
Yuya Kato <yuyakato@gmail.com>
2009-08-18 23:23:15 +0900

    部分レーダー画像のサイズを41x41に変更し、センターマークを追加

--- image_handler.py
+++ image_handler.py
@@ -47,10 +47,10 @@ class WholeImage(webapp.RequestHandler):
     time    = timeutil.yyyymmddhhnn_to_datetime(time)
     ordinal = int(ordinal)
 
-    image = amayadori.get_image(area, time, ordinal)
+    pngbin = amayadori.get_image(area, time, ordinal)
 
     self.response.headers["Content-Type"] = "image/png"
-    self.response.out.write(image)
+    self.response.out.write(pngbin)
 
 
 class PartialReducedImage(webapp.RequestHandler):
@@ -60,24 +60,38 @@ class PartialReducedImage(webapp.RequestHandler):
     ordinal = int(ordinal)
     x, y    = int(x), int(y)
 
-    image = amayadori.get_image(area, time, ordinal)
+    sx = x - 20 if x >= 20 else 0
+    sy = y - 20 if y >= 20 else 0
+    dx = 41
+    dy = 41
 
-    sx = x - 15 if x >= 15 else 0
-    sy = y - 15 if y >= 15 else 0
-    dx = 31
-    dy = 31
+    pngbin = amayadori.get_image(area, time, ordinal)
+    pngimg = png.Png8bitPalette.load(pngbin)
 
-    pngimg = png.Png8bitPalette.load(image)
-
-    gifimg = giflib.Image(31, 31, 8)
+    gifimg = giflib.Image(dx, dy, 8)
     gifimg.allocate_color((192, 192, 192))
 
-    for yy in range(dy):
-      for xx in range(dx):
+    for yy in xrange(dy):
+      for xx in xrange(dx):
         rgb1  = pngimg.get_color((sx + xx, sy + yy))
         rgb2  = jmalib.RadarNowCast.color_reduction(rgb1)
         index = gifimg.allocate_color(rgb2)
         gifimg.set_pixel((xx, yy), index)
+
+    border = gifimg.allocate_color((255, 255, 255))
+    for xx in xrange(dx):
+      gifimg.set_pixel((xx, 0     ), border)
+      gifimg.set_pixel((xx, dy - 1), border)
+    for yy in xrange(dy):
+      gifimg.set_pixel((0     , yy), border)
+      gifimg.set_pixel((dx - 1, yy), border)
+
+    center = gifimg.allocate_color((255, 255, 255))
+    for i in xrange(10):
+      gifimg.set_pixel((i + 1,      dy / 2), center)
+      gifimg.set_pixel((dx - i - 2, dy / 2), center)
+      gifimg.set_pixel((dx / 2, i + 1     ), center)
+      gifimg.set_pixel((dx / 2, dy - i - 2), center)
 
     self.response.headers["Content-Type"] = "image/gif"
     gifimg.write(self.response.out)
