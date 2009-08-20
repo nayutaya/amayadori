34306cfa885fbfddfc8b1cd3aa3ee3900d8131fa
Yuya Kato <yuyakato@gmail.com>
2009-08-20 19:51:10 +0900

    ソフトバンクの位置情報を変換する処理を追加

--- latlngutil.py
+++ latlngutil.py
@@ -11,3 +11,13 @@ def dms_to_deg(dms):
   min    = float(match.group(3))
   sec    = float(match.group(4))
   return sign * (deg + (min / 60) + (sec / 3600))
+
+def softbank_pos_to_dms_dms(pos):
+  regexp = re.compile(r"^([NS])(\d+\.\d+\.\d+\.\d+)([EW])(\d+\.\d+\.\d+\.\d+)$")
+  match  = regexp.match(pos);
+  if match is None: return (None, None)
+
+  latdms = ("-" if match.group(1) == "S" else "") + match.group(2)
+  lngdms = ("-" if match.group(3) == "W" else "") + match.group(4)
+
+  return (latdms, lngdms)
--- latlngutil_test.py
+++ latlngutil_test.py
@@ -19,6 +19,16 @@ class TestLatLngUtility(unittest.TestCase):
     self.assertEqual(1.0, target("0.0.3600.0"))
     self.assertEqual(1.5101, target("1.30.36.36"))
 
+  def test_softbank_pos_to_dms_dms(self):
+    target = latlngutil.softbank_pos_to_dms_dms
+
+    self.assertEqual((None, None), target(""))
+    self.assertEqual(( "0.0.0.0",  "0.0.0.0"), target("N0.0.0.0E0.0.0.0"))
+    self.assertEqual(("-0.0.0.0", "-0.0.0.0"), target("S0.0.0.0W0.0.0.0"))
+
+    self.assertEqual(("12.34.56.78", "0.0.0.0"), target("N12.34.56.78E0.0.0.0"))
+    self.assertEqual(("0.0.0.0", "12.34.56.78"), target("N0.0.0.0E12.34.56.78"))
+
 
 if __name__ == "__main__":
   unittest.main()
