1aa250c60c391ff5a5f5437345a6ddc167796a99
Yuya Kato <yuyakato@gmail.com>
2009-08-20 14:26:20 +0900

    リファクタリング

--- amayadori.py
+++ amayadori.py
@@ -24,39 +24,29 @@ def fetcher(url):
 def get_current_time():
   return timeutil.get_per_minute_time(timeutil.get_jst_now())
 
-def get_radar_time(current_time = None):
-  current_time = current_time or get_current_time()
-
-  key   = "radar_time_" + current_time.strftime("%Y%m%d%H%M")
+def cache(key, creator, time):
   value = memcache.get(key)
-  if value != None: return value
-
-  value = jmalib.get_current_radar_time(fetcher)
-  memcache.add(key, value, 120)
-
+  if value is None:
+    value = creator()
+    memcache.add(key, value, time)
   return value
+
+def get_radar_time(current_time = None):
+  time = current_time or get_current_time()
+  key  = "radar_time_" + time.strftime("%Y%m%d%H%M")
+  proc = lambda: jmalib.get_current_radar_time(fetcher)
+  return cache(key, proc, 120)
 
 def get_nowcast_time(current_time = None):
-  current_time = current_time or get_current_time()
-
-  key   = "nowcast_time_" + current_time.strftime("%Y%m%d%H%M")
-  value = memcache.get(key)
-  if value != None: return value
-
-  value = jmalib.get_current_nowcast_time(fetcher)
-  memcache.add(key, value, 120)
-
-  return value
+  time = current_time or get_current_time()
+  key  = "nowcast_time_" + time.strftime("%Y%m%d%H%M")
+  proc = lambda: jmalib.get_current_nowcast_time(fetcher)
+  return cache(key, proc, 120)
 
 def get_image(area, time, ordinal):
-  key   = "image_%03i_%s_%02i" % (area, time.strftime("%Y%m%d%H%M"), ordinal)
-  value = memcache.get(key)
-  if value != None: return value
-
-  value = jmalib.get_image(area, time, ordinal, fetcher)
-  memcache.add(key, value, 60 * 20)
-
-  return value
+  key = "image_%03i_%s_%02i" % (area, time.strftime("%Y%m%d%H%M"), ordinal)
+  proc = lambda: jmalib.get_image(area, time, ordinal, fetcher)
+  return cache(key, proc, 60 * 20)
 
 def get_rainfall(image, cxy):
   cx, cy = cxy
